8d8bb560e88b69f22fd04860bc0369b70b98f609
lexborelli <alexasborelli@gmail.com>
2024-01-23 14:10:58 -0500

    added 5 phrases to empty phrase array

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,14 @@
 class Game {
     constructor(missed, phrases, activePhrase) {
         this.missed = 0;
-        this.phrase = [];
+        this.phrase = [
+            'Easier said than done',
+            'Lose your marbles',
+            'Give it a shot',
+            'Never in a million years',
+            'keep your chin up',
+            'That is the last straw'
+        ];
         this.activePhrase = null; 
 
     }
